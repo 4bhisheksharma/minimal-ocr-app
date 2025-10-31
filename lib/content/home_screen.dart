5c7cbb7f7aab33b6cb92875b9217218612161ca8
Abhishek Sharma <sharmaabhi10101010@gmail.com>
2025-10-31 13:03:02 +0545

    ui: refine home screen UI; Implement code changes to enhance functionality

--- lib/content/home_screen.dart
+++ lib/content/home_screen.dart
@@ -1,4 +1,6 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:lottie/lottie.dart';
 import 'package:ocr_app/theme/app_theme.dart';
 
@@ -14,7 +16,67 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(children: [Lottie.asset('assets/json/Image.json')]),
+        child: Padding(
+          padding: const EdgeInsets.only(top: 5.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // App logo animation as JSON
+              Lottie.asset('assets/json/OCR.json', width: 200, height: 200),
+              Text(
+                'O C R',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.w500,
+                  color: MyAppTheme.primaryPurple,
+                ),
+              ),
+              const SizedBox(height: 20),
+
+              // Tap to add image from gallery
+              GestureDetector(
+                onTap: () async {
+                  final ImagePicker picker = ImagePicker();
+                  final XFile? picked = await picker.pickImage(
+                    source: ImageSource.gallery,
+                  );
+                  if (picked == null) return;
+
+                  showDialog(
+                    context: context,
+                    builder: (_) => AlertDialog(
+                      content: Image.file(File(picked.path)),
+                      actions: [
+                        TextButton(
+                          onPressed: () => Navigator.of(context).pop(),
+                          child: const Text('Close'),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+                child: Column(
+                  children: [
+                    // Animation for image picker button
+                    Lottie.asset(
+                      'assets/json/Image.json',
+                      width: 200,
+                      height: 200,
+                    ),
+                    const SizedBox(height: 8),
+                    const Text(
+                      'Tap to add image from gallery',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: MyAppTheme.primaryPurple,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
